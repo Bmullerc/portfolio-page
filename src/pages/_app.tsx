952e6834613b76aaa66f8bdaee85b098933156e5
Bernardo Muller <bernardo.muller.bm@gmail.com>
2022-11-23 20:15:20 -0300

    Pls work

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,47 +2,18 @@ import { AnimatePresence } from 'framer-motion'
 import type { AppProps } from 'next/app'
 import { Navbar } from '../components/Navbar'
 import { Socials } from '../components/Socials'
-import { useEffect, useState } from 'react'
-import { ThemeContext } from '../contexts/ThemeContext'
-import { setCookie } from 'nookies'
 
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [theme, setTheme] = useState(pageProps.CookieTheme)
-
-  useEffect(() => {
-    if (theme === "dark") {
-      document.body.classList.add('dark')
-    } else {
-      document.body.classList.remove('dark')
-    }
-  }, [theme])
-
-  function handleChangeTheme() {
-    if (theme === "light") {
-      setTheme("dark")
-      setCookie(null, "bmuller_portfolio_theme", "dark", {
-        maxAge: 86400 * 7,
-        path: '/',
-      })
-    } if (theme === "dark") {
-      setTheme("light")
-      setCookie(null, "bmuller_portfolio_theme", "light", {
-        maxAge: 86400 * 7,
-        path: '/',
-      })
-    }
-  }
-
   return (
-    <ThemeContext.Provider value={{ theme, setTheme }}>
+    <>
       <AnimatePresence mode='sync'>
         <Socials key={1} />
         <Navbar key={2} />
-        <Component {...pageProps} handleChangeTheme={handleChangeTheme} />
+        <Component {...pageProps} />
       </AnimatePresence>
-    </ThemeContext.Provider>
+    </>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,37 @@ import { Header } from "../components/Header";
 import { parseCookies, setCookie } from 'nookies'
 import { motion } from "framer-motion"
 import { Sunglasses } from "phosphor-react";
+import { useEffect, useState } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
-interface HomeProps {
-  handleChangeTheme: () => void
-}
+export default function Home({ CookieTheme }: any) {
+  const [theme, setTheme] = useState(CookieTheme)
 
-export default function Home({ handleChangeTheme }: HomeProps) {
+  useEffect(() => {
+    if (theme === "dark") {
+      document.body.classList.add('dark')
+    } else {
+      document.body.classList.remove('dark')
+    }
+  }, [theme])
+
+  function handleChangeTheme() {
+    if (theme === "light") {
+      setTheme("dark")
+      setCookie(null, "bmuller_portfolio_theme", "dark", {
+        maxAge: 86400 * 7,
+        path: '/',
+      })
+    } if (theme === "dark") {
+      setTheme("light")
+      setCookie(null, "bmuller_portfolio_theme", "light", {
+        maxAge: 86400 * 7,
+        path: '/',
+      })
+    }
+  }
   return (
-    <>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
       <Head>
         <title>B.Müller &bull; Portfolio</title>
       </Head>
@@ -29,7 +52,7 @@ export default function Home({ handleChangeTheme }: HomeProps) {
       >
         <Header />
       </motion.main>
-    </>
+    </ThemeContext.Provider>
 
   )
 }
