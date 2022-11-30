452d968f0f98318f9dfa45fcf8e931b2c173c398
Bernardo Muller <bernardo.muller.bm@gmail.com>
2022-11-30 09:15:47 -0300

    add: email send + success page

--- src/components/ContactContent.tsx
+++ src/components/ContactContent.tsx
@@ -7,23 +7,30 @@ export function ContactContent() {
       <div className="mt-10 font-body-sans text-2xl">
         You can message me on <Link className="font-bold hover:opacity-50" href="https://www.linkedin.com/in/bernardo-m%C3%BCller-086a64235/" target="_blank" rel="nofollow"> Linkedin</Link>,<Link className="font-bold hover:opacity-50" href="https://discordapp.com/users/Bmullerc#1080" target="_blank" rel="nofollow"> Discord </Link> or you can send me an <span className="font-bold">e-mail</span> bellow.
         <form
-          action="mailto:bernardo.muller.bm@gmail.com"
+          action="https://formsubmit.co/9b4974c43bc9208f39ba2d0e86b1d226"
           method="POST"
           name="email-form"
-          className="flex flex-col gap-1 pt-4 pb-20 dark:text-zinc-700"
+          className="flex flex-col gap-2 pt-4 pb-20 dark:text-zinc-700"
         >
+          <div className="flex gap-2 justify-center items-center mt-2">
+            <label htmlFor="name" className="font-semibold text-xl dark:text-zinc-300">Name:</label>
+            <input type="text" name="name" id="name" required placeholder="Your name here" className="w-full rounded-md bg-zinc-300 focus:ring-2 hover:ring-2 hover:ring-zinc-50 outline-none ring-zinc-50 transition-all duration-150 py-2 px-4 text-base mb-2" />
 
-          <label htmlFor="name" className="font-semibold text-xl dark:text-zinc-300">Name:</label>
-          <input type="text" name="name" id="name" placeholder="Your name here" className="w-full rounded-md bg-zinc-300 focus:ring-2 hover:ring-2 hover:ring-zinc-50 outline-none ring-zinc-50 transition-all duration-150 py-2 px-4 text-base mb-2" />
+            <label htmlFor="email" className="font-semibold text-xl dark:text-zinc-300">E-mail:</label>
+            <input type="email" name="email" id="email" required placeholder="Your e-mail here" className="w-full rounded-md bg-zinc-300 focus:ring-2 hover:ring-2 hover:ring-zinc-50 outline-none ring-zinc-50 transition-all duration-150 py-2 px-4 text-base mb-2" />
+            </div>
+
+          <input type="hidden" name="_next" value="http://localhost:3000/success"/>
+
 
           <label htmlFor="title" className="font-semibold text-xl dark:text-zinc-300">Topic:</label>
-          <input type="text" name="title" id="title" placeholder="What do you want to talk about?" className="w-full rounded-md bg-zinc-300 focus:ring-2 hover:ring-2 hover:ring-zinc-50 outline-none ring-zinc-50 transition-all duration-150 py-2 px-4 text-base mb-2" />
+          <input type="text" name="title" id="title" required placeholder="What do you want to talk about?" className="w-full rounded-md bg-zinc-300 focus:ring-2 hover:ring-2 hover:ring-zinc-50 outline-none ring-zinc-50 transition-all duration-150 py-2 px-4 text-base mb-2" />
 
 
           <label htmlFor="msg" className="font-semibold text-xl dark:text-zinc-300">Message:</label>
-          <input type="text" name="msg" id="msg" placeholder="Type your message here" className="w-50% h-24 rounded-md bg-zinc-300 focus:ring-2 hover:ring-2 dark:hover:ring-4 outline-none ring-zinc-50 transition-all duration-150 px-4 text-base mb-2" />
+          <input type="text" name="msg" id="msg" required placeholder="Your message here" className="w-50% h-24 rounded-md bg-zinc-300 focus:ring-2 hover:ring-2 dark:hover:ring-4 outline-none ring-zinc-50 transition-all duration-150 px-4 text-base mb-2" />
 
-          <button className="bg-zinc-600 text-zinc-300 rounded-md w-28 py-1 px-2 m-auto hover:text-zinc-600 hover:bg-zinc-300 focus:text-zinc-600 focus:bg-zinc-300">Submit</button>
+          <button className="bg-zinc-600 text-zinc-300 rounded-lg w-28 py-1 px-3 m-auto hover:text-zinc-600 hover:bg-zinc-300 focus:text-zinc-600 focus:bg-zinc-300 font-semibold">Submit</button>
         </form>
       </div>
     </div>
--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,26 +1,26 @@
-import { motion } from "framer-motion";
 import Head from "next/head";
+import { motion } from "framer-motion"
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Projects() {
+export default function Sucess() {
   return (
     <>
       <Head>
-        <title>B.Müller &bull; 404</title>
+        <title>B.Müller &bull; Success</title>
       </Head>
-      <motion.article
+      <motion.section
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         transition={{ duration: 1 }}
         className="dark:bg-zinc-800 h-screen bg-zinc-200 flex flex-col justify-center items-center font-body-sans text-zinc-700 dark:text-zinc-300">
         <Image src={"/monke.png"} alt="" width={200} height={200} className="rounded-full mb-8" />
-        <h1 className="font-title-sans font-semibold lg:text-5xl text-4xl mb-2">404, Friend</h1>
-        <p>Looks like you are in the wrong place there amigo.</p>
+        <h1 className="font-title-sans font-semibold lg:text-5xl text-4xl mb-2">Message sent.</h1>
+        <p>Nice.</p>
 
         <Link href={"/"} className="mt-4 font-bold dark:text-zinc-300 cursor-pointer hover:scale-110 hover:text-zinc-400 dark:hover:text-zinc-100 duration-200">Go back</Link>
-      </motion.article>
+      </motion.section>
     </>
   )
 }
