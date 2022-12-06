15c7fc24aef3dea2f97d6fdf30653ad474fb655a
Bernardo Muller <bernardo.muller.bm@gmail.com>
2022-12-06 12:11:37 -0300

    fix: more responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,13 @@ import { motion } from "framer-motion"
 export function Header() {
   return (
     <motion.header
-      className="h-screen lg:px-32 lg:w-2/5 w-3/5 mx-8 flex flex-col lg:flex-row place-items-center justify-center">
+      className="h-screen lg:px-32 px-8 lg:w-2/5 w-full flex flex-col lg:flex-row place-items-center justify-center pt-20 lg:pt-0">
       <motion.div
         initial={{ y: "200vh" }}
         animate={{ y: 0 }}
         exit={{ y: "-100vh" }}
         transition={{ delay: .7, duration: 1, type: "spring", damping: 25, stiffness: 250 }}
-        className="sm:px-8 px-4 flex flex-col gap-3 font-title-sans font-bold border-r-8 border-zinc-500"
+        className="px-4 flex flex-col gap-3 font-title-sans font-bold border-r-8 border-zinc-500"
       >
         <h1 className="lg:text-7xl text-5xl text-zinc-500">
           <span className="text-zinc-900 dark:text-zinc-50">Hi.</span> I am <span className="text-zinc-900 dark:text-zinc-50">Bernardo Müller</span>.
